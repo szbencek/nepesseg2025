bc19b4f7c9adf69d4fbbc7058ab587ae3964628f
Szabó Bence Kálmán <bence17474@gmail.com>
2026-09-15 14:33:54 +0200

    Település típusok menüpont részleges megvalósítása

--- nepesseg2025.py
+++ nepesseg2025.py
@@ -1,3 +1,5 @@
+import os
+
 nepesseg_adatok = []
 
 with open("lakossag_2025.csv", "r", encoding="UTF-8") as forrasfajl:
@@ -21,8 +23,23 @@ def menupont_valaszto(menupont):
     match menupont:
         case "1": megye_adatok()
         case "2": telepules_tipus()
-        case "3": pass
-        case "x": main()
+        case "x": return
+
+def telepules_tipus_menupontok():
+    telepules_tipusok = []
+
+    for n in nepesseg_adatok:
+        if n["tipus"] in telepules_tipusok:
+            continue
+        else:
+            telepules_tipusok.append(n["tipus"])
+
+    for index, tipus in enumerate(telepules_tipusok):
+        print(f"[{index + 1}] {tipus}")
+
+    telepules = input("Kérem a település típusának sorszámát: ")
+
+    return telepules_tipusok[int(telepules) - 1]
         
 
 
@@ -38,22 +55,38 @@ def megye_adatok():
             telepulesek_szama += 1
             lakosok_szama += n["ferfi"] + n["no"]
 
-            if n["tipus"].lower() == "város":
+            if n["tipus"].lower() == "város" or n["tipus"].lower() == "vármegyei jogú város" or n["tipus"].lower() == "vármegye székhely":
                 varosok_lakoinak_szama += n["ferfi"] + n["no"]
 
     print(f"Települések száma: {telepulesek_szama}")
-    print(f"Összes lakos: {lakosok_szama}")
-    print(f"Városok lakosai összesen: {varosok_lakoinak_szama}")
+    print(f"Összes lakos: {lakosok_szama} fő")
+    print(f"Városok lakosai összesen: {varosok_lakoinak_szama} fő")
+
 
 def telepules_tipus():
-    pass
+    print("Válasszon településtípust!")
+    telepules_tipus = telepules_tipus_menupontok()
+
+    terminal_height = os.get_terminal_size().lines
+
+    telepules_adatok = []
+
+    for n in nepesseg_adatok:
+            if n["tipus"].lower() == telepules_tipus.lower():
+                telepules_adatok.append(f"{n['telepules']} - {n['ferfi'] + n['no']} fő")
+
+    for i in range(terminal_height - 1):
+        if i < len(telepules_adatok):
+            print(telepules_adatok[i])
+        else:
+            break
+
 
 
 def main():
     print("----- Népesség 2026 -----".center(40))
     print("[1] Megye adatai")
     print("[2] Település típusa")
-    print("[3]")
     print("[X] Kilépés")
 
     menupont_bevitel = input("Írd be a választott menüpont sorszámát: ")
